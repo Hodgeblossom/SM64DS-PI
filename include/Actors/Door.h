e65a49afb2f052b87e546997de7b76fe16885da7
pants64DS <pants64DS@users.noreply.github.com>
2025-12-04 00:27:56 +0200

    Add explicit overrides to the Door struct

--- include/Actors/Door.h
+++ include/Actors/Door.h
@@ -10,12 +10,12 @@ struct Door : Actor
 	u32 unk140;
 	u32 unk144;
 
-	virtual s32 InitResources();
-	virtual s32 CleanupResources();
-	virtual s32 Behavior();
-	virtual s32 Render();
-	virtual void OnPendingDestroy();
-	virtual ~Door();
+	virtual s32 InitResources() override;
+	virtual s32 CleanupResources() override;
+	virtual s32 Behavior() override;
+	virtual s32 Render() override;
+	virtual void OnPendingDestroy() override;
+	virtual ~Door() override;
 
 	static bool StopBowserMessageSound();
 	static bool ContinueBowserMessageSound();
